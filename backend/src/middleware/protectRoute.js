0e3a6478fc53e47731b1e3f782a5bddeec9e9c6f
project <sandeepsharmadec2018@gmail.com>
2026-02-04 10:51:01 +0530

    session routes and controllers added

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -3,22 +3,22 @@ import User from "../models/User.js";
 
 export const protectRoute = [
     requireAuth(),
-    async (req, res, next) =>{
+    async (req, res, next) => {
         try {
             const clerkId = req.auth().userId;
-            if(!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token"});
+            if (!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token" });
 
-                //find user in db by clerk ID
-                const user = await User.findOne({clerkId});
+            //find user in db by clerk ID
+            const user = await User.findOne({ clerkId });
 
-                if(!user) return res.status(404).json({message:"User not found"});
+            if (!user) return res.status(404).json({ message: "User not found" });
 
-                    // attach user to req
-                    req.user= user;
-                    next();
+            // attach user to req
+            req.user = user;
+            next();
         } catch (error) {
             console.error("Error in protectRoute middleware:", error);
-            res.status(500).json({message:"internal server error"});
+            res.status(500).json({ message: "internal server error" });
         }
     },
 ];
